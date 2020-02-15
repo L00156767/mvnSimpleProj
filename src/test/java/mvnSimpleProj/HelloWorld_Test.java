552ef447c3a21e2811bd94277da14c26da250361
L00156767 <L00156767@student.lyit.ie>
2020-02-15 08:58:47 -0700

    Test jenkins build again

--- src/test/java/mvnSimpleProj/HelloWorld_Test.java
+++ src/test/java/mvnSimpleProj/HelloWorld_Test.java
@@ -25,7 +25,7 @@ public class HelloWorld_Test {
 				ByteArrayOutputStream();
 				System.setOut(new PrintStream(outContent)); //make the output go to a stream instead of the console
 				App.main(null); //call main in the App file
-				//check if the output is "Hello LYIT!" and a new line.
+				//check if the output is "Hello LYIT!!!" and a new line.
 				assertEquals("Hello LYIT!!" + System.getProperty("line.separator"),
 				outContent.toString());
 	}
